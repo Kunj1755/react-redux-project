01b71d93a8db3bf861484c9b9d4b994dc6bab93d
KB <kb@kb.com>
2021-02-06 16:00:03 +0530

    5. Introducing Action Creators

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,11 +1,11 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actions';
 
 const initialState = {
     counter: 0
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case actionTypes.INCREMENT:
             const newState = Object.assign({}, state);
             newState.counter = state.counter + 1;
